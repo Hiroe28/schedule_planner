998fd98c76d091052ee986d831487ec4aa4c0afb
Hiroe28 <hiroe28@gmail.com>
2023-08-22 10:56:24 +0900

    永続化実施

--- events.py
+++ events.py
@@ -1,7 +1,23 @@
+import pickle
 
+DATA_FILE = 'events_db.pkl'
 
 # イベントデータを保持するディクショナリ
 events_db = {}
+
+
+def save_data():
+    with open(DATA_FILE, 'wb') as f:
+        pickle.dump(events_db, f)
+
+
+def load_data():
+    global events_db
+    try:
+        with open(DATA_FILE, 'rb') as f:
+            events_db = pickle.load(f)
+    except FileNotFoundError:
+        events_db = {}
 
 
 def create_event(event_name, memo, dates):
@@ -12,6 +28,7 @@ def create_event(event_name, memo, dates):
         'dates': dates,
         'participants': {}
     }
+    save_data()
     return event_id
 
 
@@ -24,8 +41,13 @@ def add_participant(event_id, name, availability, comment):
         'availability': availability,
         'comment': comment
     }
+    save_data()
 
 
 def remove_participant(event_id, name):
     if name in events_db[event_id]['participants']:
         del events_db[event_id]['participants'][name]
+    save_data()
+
+# アプリケーション開始時にデータを読み込みます
+load_data()
